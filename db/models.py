9a9c1a0fe4a74dc5c4f6a1254605d7b3fbd57500
Yan <marynetsyy@gmail.com>
2026-09-16 11:17:05 +0300

    solution

--- db/models.py
+++ db/models.py
@@ -80,7 +80,7 @@ class Order(models.Model):
     )
 
     def __str__(self) -> str:
-        return f"{self.created_at}"
+        return f"<Order: {self.created_at}>"
 
     class Meta:
         ordering = ["-created_at"]
